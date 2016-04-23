471e4240db3820b74b0424e2bc831a057539292e
DandiW <dwang15@icloud.com>
2016-04-22 23:55:52 -0500

    Add Microsoft api key in

---
+++ face.py
@@ -0,0 +1,5 @@
+# Microsoft api - Facial emotion anaysis
+
+_url = 'https://api.projectoxford.ai/emotion/v1.0/recognize'
+_key = 981cde578f8540b1957f8908b87c6c09 #Here you have to paste your primary key
+_maxNumRetries = 10
